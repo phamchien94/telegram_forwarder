dfee57cee3ea1a2f8235d2dca149bbba79d3d01f
Ha Manh Dong <dongha.27995@gmail.com>
2022-07-02 12:58:27 +0000

    Add forward for list of channel

--- forwarder.py
+++ forwarder.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pyrogram import Client, filters, idle
 
 from settings import (
@@ -6,14 +7,26 @@ from settings import (
 
 app = Client(TG_SESSION, TG_API_ID, TG_API_HASH)
 
+chat_ids = FORWARD_CHAT_IDS
+dest_chat_ids = TARGET_CHAT_ID
 
-@app.on_message(filters.chat(FORWARD_CHAT_IDS))
+@app.on_message(filters.chat(chat_ids))
 def handler(client, message):
-    client.forward_messages(
-        chat_id=TARGET_CHAT_ID,
-        from_chat_id=message.chat.id,
-        message_ids=message.message_id
-    )
+    is_duplicated = False
+    if message.edit_date:
+        d = datetime.datetime.strptime(message.date, '%Y-%m-%d %H:%M:%S')
+        e = datetime.datetime.strptime(message.edit_date, '%Y-%m-%d %H:%M:%S')
+        delta = (e - d).total_seconds()
+        if delta < 5:
+            is_duplicated = True
+    if not is_duplicated:
+        for target in dest_chat_ids:
+            client.forward_messages(
+                chat_id=target,
+                from_chat_id=message.chat.id,
+                message_ids=message.message_id
+            )
+        is_duplicated = False
 
 
 app.start()
--- settings.py
+++ settings.py
@@ -1,7 +1,13 @@
 import os
 
-TG_API_ID = int(os.environ.get("TG_API_ID"))
-TG_API_HASH = os.environ.get("TG_API_HASH")
-TG_SESSION = os.environ.get("TG_SESSION")
-FORWARD_CHAT_IDS = [int(os.environ.get("FORWARD_CHAT_IDS"))]
-TARGET_CHAT_ID = int(os.environ.get("TARGET_CHAT_ID"))
+TG_API_ID = 123456
+TG_API_HASH = "XXX"
+TG_SESSION = "YYY"
+FORWARD_CHAT_IDS=[ID_Channel_1, ID_Channel_2]
+TARGET_CHAT_ID=[ID_Channel_3]
+
+#TG_API_ID = int(os.environ.get("TG_API_ID"))
+#TG_API_HASH = os.environ.get("TG_API_HASH")
+#TG_SESSION = os.environ.get("TG_SESSION")
+#FORWARD_CHAT_IDS = os.environ.get("FORWARD_CHAT_IDS")
+#TARGET_CHAT_ID = os.environ.get("TARGET_CHAT_ID") 
